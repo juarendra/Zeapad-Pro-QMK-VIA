5da20a92735b5bbb449b3a920ef118be098791fa
juarendra <jrjuarendra@gmail.com>
2024-02-01 13:46:08 +0700

    update

---
+++ FIRMWARE/zeapadpro_stm32___/config.h
@@ -0,0 +1,49 @@
+#pragma once
+
+//#include "config_common.h"
+#define MATRIX_ROWS  4
+#define MATRIX_COLS  5
+
+// wiring of each half
+#define MATRIX_ROW_PINS       { A6, A7, B0, B1 }
+#define MATRIX_COL_PINS       { A2, A3, A4, A5, A1} // A virtual pin is needed for the encoder key matrix in via.
+
+#define DIODE_DIRECTION COL2ROW
+
+#define TAP_CODE_DELAY  10
+//#define ENCODER_DIRECTION_FLIP
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+#define ENCODERS_PAD_A { C14 ,B15, B8}
+#define ENCODERS_PAD_B { C13 ,B13, B12}
+#define ENCODER_RESOLUTIONS       { 4 , 4, 4}
+/* disable these deprecated features by default */
+
+#define WS2812_DI_PIN B14 //D3 - underglow C7 - backlight
+#define RGBLED_NUM 14    // Number of L
+
+// ┌─────────────────────────────────────────────────┐
+// │ o l e d s                                       │
+// └─────────────────────────────────────────────────┘
+
+#ifdef OLED_ENABLE
+#    define OLED_DISPLAY_128X64
+#    define SPLIT_OLED_ENABLE
+#endif
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_SWIRL+2
+
+// Disabling some of these is a good way to save flash space.
+#    define RGBLIGHT_EFFECT_ALTERNATING     // 108
+#    define RGBLIGHT_EFFECT_RGB_TEST        // 158
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD    // 160
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT // 168
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL   // 192
+#    define RGBLIGHT_EFFECT_BREATHING       // 348
+#    define RGBLIGHT_EFFECT_KNIGHT          // 336
+#    define RGBLIGHT_EFFECT_SNAKE           // 406
+#    define RGBLIGHT_EFFECT_CHRISTMAS       // 508
+/*
+#define RGBLIGHT_EFFECT_TWINKLE         // 1156
+*/
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Zeapad-Pro-QMK-VIA
+
+## Spesification
+- STM32F401 as Microcontroller
+- QMK Firmware
+- Support VIA, all key and knob can proggrammed
+- RGB Underglow
+- 4x Layer 
+- 16x Switch , 3 Knob Encoder and 1 OLED LAyer
+- Hotswap Switch
+- 3D Case 
+- USB type C
+
+## Download VIA
+Link Download VIA(Pilih sesuai OS) : https://github.com/the-via/releases/releases
+VIA WEB VERSION : https://usevia.app/
+
+## load JSON File
+- Connect your macropad to PC
+- Open VIA
+- In Tab Setting, enable "Show Design Tab"
+- Open Design Tab
+- Load file with name "zeapad_via_definitions.json" 
+- Open Configure Tab to setting your macropad
+- If nothing happend , do it again from first
+
+## Preview Hardware
+
+## Preview VIA
+[![Positron Electronic on YouTube](http://img.youtube.com/vi/qvMjg3LCvkY/0.jpg)](http://www.youtube.com/watch?v=qvMjg3LCvkY "zeapad stm32")
