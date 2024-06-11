c9aa094cc95da1dd980b68941f47354d7d5545c1
juarendra <jrjuarendra@gmail.com>
2024-06-11 11:00:32 +0700

    FIX: update license

--- FIRMWARE/OLD/zeapadpro_stm32___/mcuconf.h
+++ FIRMWARE/OLD/zeapadpro_stm32___/mcuconf.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 Nick Brassel (tzarc)
+/* Copyright 2024 Juarendra Ramadhani(jrjruarendra@gmail.com)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
